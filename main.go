eedd28a29cc55b695c220538f1e10285e54e3a29
Himanshu Sekhar Das <61196311+dasHimanshuSekhar@users.noreply.github.com>
2023-01-23 19:14:36 +0530

    done assesment
    done assesment

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+const port = 8080
+
+func main() {
+
+	// fetching responses
+	res, err := http.Get("https://random-data-api.com/api/v2/banks")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	// convert from byte to json
+	JsonRes, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Println(string(JsonRes))
+
+	requiredDetailsMap := map[string]string{}
+
+	json.Unmarshal([]byte(JsonRes), &requiredDetailsMap)
+
+	fmt.Println("iban: " + requiredDetailsMap["iban"])
+	fmt.Println("bank_name: " + requiredDetailsMap["bank_name"])
+	fmt.Println("routing_number: " + requiredDetailsMap["routing_number"])
+}
